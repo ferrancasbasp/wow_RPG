fda0cc5e93d2a8edd5a6110be9fa3ae542b231b0
Ferran Casbas <ferran.casbas@revolut.com>
2026-07-31 10:31:38 +0000

    Firebase config: credenciales del proyecto rpgwow-118f7

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,11 +1,12 @@
 const firebaseConfig = {
-  apiKey: "TU_API_KEY",
-  authDomain: "TU_PROYECTO.firebaseapp.com",
-  databaseURL: "https://TU_PROYECTO-default-rtdb.firebaseio.com",
-  projectId: "TU_PROYECTO",
-  storageBucket: "TU_PROYECTO.appspot.com",
-  messagingSenderId: "TU_SENDER_ID",
-  appId: "TU_APP_ID"
+  apiKey: "AIzaSyCxsMUUHvw_LQrl24VgDtJiperHF2rRL_Y",
+  authDomain: "rpgwow-118f7.firebaseapp.com",
+  databaseURL: "https://rpgwow-118f7-default-rtdb.firebaseio.com",
+  projectId: "rpgwow-118f7",
+  storageBucket: "rpgwow-118f7.firebasestorage.app",
+  messagingSenderId: "408168433969",
+  appId: "1:408168433969:web:d1f8521365c247ac934810",
+  measurementId: "G-STV8D02FJV"
 };
 
 if (typeof firebase !== 'undefined' && !firebase.apps.length) {
