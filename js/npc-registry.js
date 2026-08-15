336fa24d39c9510431f3a3f29087ae1e52d04e0c
Ferran Casbas <ferran.casbas@revolut.com>
2026-08-15 15:37:10 +0000

    NPCs: WoW Classic boars with English names + stats rebalance
    - 4 boars matching WoW zones: Elwynn (3-5) and Redridge (12-15)
    - Mottled Boar (3), Dire Boar (5), Great Goretusk (12), Elder Goretusk (15)
    - HP/armor/damage adjusted to WoW Classic values
    - New images for level 12 and 15 boars
    - Attack names in English: Charge, Bite, Gore, Savage Bite, Trample

--- js/npc-registry.js
+++ js/npc-registry.js
@@ -1,43 +1,59 @@
 window.NPC_REGISTRY = window.NPC_REGISTRY || {};
 
-window.NPC_REGISTRY['young_boar'] = {
-  name: 'Cerdo Joven',
+window.NPC_REGISTRY['mottled_boar'] = {
+  name: 'Mottled Boar',
   icon: '🐗',
   imageUrl: 'img/enemies/boar_young.jpg',
-  level: 2,
-  hp: 120,
-  armor: 15,
+  level: 3,
+  hp: 85,
+  armor: 10,
   magicResist: 0,
   attacks: [
-    { name: 'Embestida', min: 5, max: 8, type: 'physical' },
+    { name: 'Charge', min: 4, max: 7, type: 'physical' },
+    { name: 'Bite', min: 3, max: 5, type: 'physical' },
   ],
 };
 
 window.NPC_REGISTRY['dire_boar'] = {
-  name: 'Jabalí Feroz',
+  name: 'Dire Boar',
   icon: '🐗',
   imageUrl: 'img/enemies/boar_dire.jpg',
   level: 5,
-  hp: 280,
-  armor: 30,
+  hp: 152,
+  armor: 20,
   magicResist: 5,
   attacks: [
-    { name: 'Embestida', min: 12, max: 18, type: 'physical' },
-    { name: 'Mordisco', min: 8, max: 14, type: 'physical' },
+    { name: 'Charge', min: 7, max: 12, type: 'physical' },
+    { name: 'Bite', min: 5, max: 9, type: 'physical' },
   ],
 };
 
-window.NPC_REGISTRY['enraged_boar'] = {
-  name: 'Jabalí Enfurecido',
+window.NPC_REGISTRY['great_goretusk'] = {
+  name: 'Great Goretusk',
   icon: '🐗',
-  imageUrl: 'img/enemies/boar_enraged.jpg',
-  level: 10,
-  hp: 650,
+  imageUrl: 'img/enemies/boar_goretusk.jpg',
+  level: 12,
+  hp: 412,
+  armor: 40,
+  magicResist: 10,
+  attacks: [
+    { name: 'Gore', min: 14, max: 22, type: 'physical' },
+    { name: 'Savage Bite', min: 10, max: 18, type: 'physical' },
+    { name: 'Trample', min: 8, max: 26, type: 'physical' },
+  ],
+};
+
+window.NPC_REGISTRY['elder_goretusk'] = {
+  name: 'Elder Goretusk',
+  icon: '🐗',
+  imageUrl: 'img/enemies/boar_elder_goretusk.jpg',
+  level: 15,
+  hp: 585,
   armor: 55,
   magicResist: 15,
   attacks: [
-    { name: 'Embestida Salvaje', min: 25, max: 38, type: 'physical' },
-    { name: 'Mordisco Furioso', min: 18, max: 30, type: 'physical' },
-    { name: 'Aplastar', min: 15, max: 45, type: 'physical' },
+    { name: 'Gore', min: 20, max: 32, type: 'physical' },
+    { name: 'Savage Bite', min: 15, max: 25, type: 'physical' },
+    { name: 'Trample', min: 12, max: 38, type: 'physical' },
   ],
 };
